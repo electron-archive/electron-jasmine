cebb37a48ecb3ad838204cd64232548f62fdbc05
Ben Ogle <ogle.ben@gmail.com>
2015-07-22 22:10:45 -0700

    Can require this from other electron apps

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-var Application = require('./src/browser/application')
-application = new Application
+var TestApplication = require('./src/browser/test-application')
+
+// FIXME: crappy way to tell if it's being run standalone or required
+if(!module.parent.parent || !module.parent.parent.children) {
+  new TestApplication
+}
+else {
+  module.exports = {
+    TestApplication: TestApplication
+  }
+}
